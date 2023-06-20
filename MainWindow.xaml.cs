954303f464ba507f208ccae1e8cc949952b9d359
Diego Cortez Carreon <c.diego2004@gmail.com>
2023-06-20 12:00:08 -0500

    ACTUAL20062023

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -146,8 +146,7 @@ namespace Calculadora
         {
             try
             {
-                double valor = 9;
-                TxtBoxResul.Text += valor.ToString();
+                HandleNumbers("9");
             }
             catch (Exception)
             {
@@ -187,7 +186,7 @@ namespace Calculadora
         {
             try
             {
-                TxtBoxResul.Text = "";
+                TxtBoxResul.Clear();
             }
             catch (Exception)
             {
@@ -200,7 +199,7 @@ namespace Calculadora
         {
             try
             {
-                
+                HandleOperators("+");
             }
             catch (Exception)
             {
@@ -208,12 +207,47 @@ namespace Calculadora
                 throw (new Exception("ERROR"));
             }
         }
+        private void BtnResta_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                HandleOperators("-");
+            }
+            catch (Exception)
+            {
 
+                throw (new Exception("ERROR"));
+            }
+        }
+        private void BtnMult_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                HandleOperators("*");
+            }
+            catch (Exception)
+            {
+
+                throw (new Exception("ERROR"));
+            }
+        }
+        private void BtnDiv_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                HandleOperators("/");
+            }
+            catch (Exception)
+            {
+
+                throw (new Exception("ERROR"));
+            }
+        }
         private void BtnResul_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                
+                HandleEquals(TxtBoxResul.Text);
             }
             catch (Exception)
             {
@@ -238,16 +272,79 @@ namespace Calculadora
             {
                 return true;
             }
-            return false;
             return PosOperador == "+" || PosOperador == "-" || PosOperador == "*" || PosOperador == "/";
         }
         private void HandleOperators(string value)
         {
-            if (!String.IsNullOrEmpty(TxtBoxResul.Text))
+            if (!String.IsNullOrEmpty(TxtBoxResul.Text) && !ContainsOtherOperators(TxtBoxResul.Text))
             {
                 TxtBoxResul.Text += value;
             }
         }
-
+        private bool ContainsOtherOperators(string screenContent)
+        {
+            return screenContent.Contains("+") || screenContent.Contains("-") || screenContent.Contains("*") || screenContent.Contains("/");
+        }
+        private string FindOperador(string screenContent)
+        {
+            foreach(char c in screenContent)
+            {
+                if (IsOperator(c.ToString()))
+                {
+                    return c.ToString();
+                }
+            }
+            return screenContent;
+        }
+        private void HandleEquals(string screenContent)
+        {
+            string op = FindOperador(screenContent);
+            if (!String.IsNullOrEmpty(op))
+            {
+                switch (op)
+                {
+                    case "+":
+                        TxtBoxResul.Text = Sum();
+                        break;
+                    case "-":
+                        TxtBoxResul.Text = Rest();
+                        break;
+                    case "*":
+                        TxtBoxResul.Text = Mult();
+                        break;
+                    case "/":
+                        TxtBoxResul.Text = Div();
+                        break;
+                }
+            }
+        }
+        private string Sum()
+        {
+            string[] numbers = TxtBoxResul.Text.Split("+");
+            double.TryParse(numbers[0], out double n1);
+            double.TryParse(numbers[1], out double n2);
+            return Math.Round(n1 + n2, 12).ToString();
+        }
+        private string Rest()
+        {
+            string[] numbers = TxtBoxResul.Text.Split("-");
+            double.TryParse(numbers[0], out double n1);
+            double.TryParse(numbers[1], out double n2);
+            return Math.Round(n1 - n2, 12).ToString();
+        }
+        private string Mult()
+        {
+            string[] numbers = TxtBoxResul.Text.Split("*");
+            double.TryParse(numbers[0], out double n1);
+            double.TryParse(numbers[1], out double n2);
+            return Math.Round(n1 * n2, 12).ToString();
+        }
+        private string Div()
+        {
+            string[] numbers = TxtBoxResul.Text.Split("/");
+            double.TryParse(numbers[0], out double n1);
+            double.TryParse(numbers[1], out double n2);
+            return Math.Round(n1 / n2, 12).ToString();
+        }
     }
 }
